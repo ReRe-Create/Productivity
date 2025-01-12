7903db85ebe491c805626d4c037943c1b4adb10f
Reanna <liureanna@gmail.comgit config --global user.email liureanna@gmail.comgit config --global user.email liureanna@gmail.com>
2025-01-12 00:19:53 -0500

    Progress Bar Update

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-function addTask(){
-    const userInput = document.getElementById("userInput");
-    const list = document.getElementById("list");
+const userInput = document.getElementById("userInput");
+const list = document.getElementById("list");
+
+ function addTask(){
     
     if(userInput.value === ""){
-        alert ("You must include a task!");
-    }
+            alert ("You must include a task!");
+        }
+    
     else{
         let li = document.createElement("li");
         li.innerHTML = userInput.value;
@@ -14,13 +16,13 @@ function addTask(){
         li.appendChild(span);
     }
     userInput.value= "";
+    updateProgress();
     saveData();
 }
 
 userInput.addEventListener("keypress", function(event){
     if (event.key === "Enter") {
         addTask();
-        saveData();
     }
   });
 
@@ -28,22 +30,61 @@ list.addEventListener("click", function (e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle ("checked");
         saveData();
+        updateProgress();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
         saveData();
+        updateProgress();
     }
 },false);
 
 function saveData(){
     localStorage.setItem("data", list.innerHTML);
-    updateProgress();
 }
 
 function showTask(){
     list.innerHTML = localStorage.getItem("data");
+    updateProgress();
 }
+
+//update progress bar
+
+function count_Checked(){
+    const task = list.getElementsByTagName("LI");
+    let count = 0;
+    for (let i = 0; i < task.length; i++){
+        if(task[i].classList.contains("checked")){
+            count++;
+        }
+    }
+    return count;
+}
+
+function updateProgress(){
+    const task = list.getElementsByTagName("LI");
+    const checked= count_Checked();
+
+    if (task.length == 0){
+        updateProgressBar(0);
+        return;
+    }
+
+    else{
+
+        const progress_val=(checked/task.length)*100;
+        updateProgressBar(progress_val);
+    }
+}
+
+function updateProgressBar(value){
+    value=Math.round(value);
+
+    const progressFill= document.querySelector(".progress_fill");
+    const progressNum= document.querySelector(".progress_num");
+
+    progressFill.style.width= `${value}%`;
+    progressNum.textContent= `${value}% completed`;
+    }
+    
 showTask();
-
-
-//progress bar
